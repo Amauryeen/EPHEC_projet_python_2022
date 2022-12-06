378a95f82f3ad7884c16f19d500e8ef41c6f9683
Martin-Hayot <m.hayot@students.ephec.be>
2022-12-06 01:31:32 +0100

    Update gui

--- guiOOP.py
+++ guiOOP.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from kotcount import *
+from tkinter.messagebox import showinfo
 
 
 class App(Tk):
@@ -7,22 +9,16 @@ class App(Tk):
         super().__init__()
         # configure the window
         self.title("kotcount")
-        self.resizable(width=False, height=True)
-        # window size
-        self.window_width = 400
-        self.window_height = 400
-
-        # get the screen dimension
-        self.screen_width = self.winfo_screenwidth()
-        self.screen_height = self.winfo_screenheight()
-
-        # find the center point
-        center_x = int(self.screen_width / 2 - self.window_width / 2)
-        center_y = int(self.screen_height / 2 - self.window_height / 2)
-
-        # set the position of the window to the center of the screen
-        self.geometry(f'{self.window_width}x{self.window_height}+{center_x}+{center_y}')
+        self.minsize(width=400, height=400)
         self.create_welcome_frame()
+
+    @staticmethod
+    def get_kot(self):
+        with open("koters.json", 'r') as json_r:
+            data = json.load(json_r)
+            kot = Kot(data["kot_name"])
+            for person in data["koters_list"]:
+                Person(person)
 
     def hide_all_frames(self):
         for widget in App.winfo_children(self):
@@ -31,18 +27,28 @@ class App(Tk):
             widget.destroy()
         for widget in MealFrame.winfo_children(self):
             widget.destroy()
+        for widget in ListMealFrame.winfo_children(self):
+            widget.destroy()
 
     def create_menu(self):
         self.menubar = Menu(self)
         self.file_menu = Menu(self.menubar, tearoff=0)
         self.kot_menu = Menu(self.menubar, tearoff=0)
         self.meal_menu = Menu(self.menubar, tearoff=0)
+        self.list_meal_menu = Menu(self.menubar, tearoff=0)
+        self.info_menu = Menu(self.menubar, tearoff=0)
         self.config(menu=self.menubar)
         self.file_menu.add_command(label="Acceuil", command=self.create_welcome_frame)
         self.file_menu.add_command(label='Exit', command=self.destroy)
         self.menubar.add_cascade(label="App", menu=self.file_menu)
         self.menubar.add_cascade(label="Kot", menu=self.kot_menu)
         self.kot_menu.add_command(label="Créer le kot", command=self.create_kot_frame)
+        self.menubar.add_cascade(label="Repas", menu=self.meal_menu)
+        self.meal_menu.add_command(label="Ouvrir", command=self.create_meal_frame)
+        self.menubar.add_cascade(label="Liste repas", menu=self.list_meal_menu)
+        self.list_meal_menu.add_command(label="Ouvrir", command=self.create_list_meal_frame)
+        self.menubar.add_cascade(label="Info",menu=self.info_menu)
+        self.info_menu.add_command(label="Ouvrir", command=self.create_info_frame)
 
     def create_kot_frame(self):
         self.hide_all_frames()
@@ -55,14 +61,39 @@ class App(Tk):
         for v in kot_frame.koters_list:
             Label(self, text=v).pack()
 
+    def create_meal_frame(self):
+        self.hide_all_frames()
+        meal_frame = MealFrame(self)
+        meal_frame.pack()
+        self.create_menu()
+
+    def create_list_meal_frame(self):
+        self.hide_all_frames()
+        list_meal_frame = ListMealFrame(self)
+        list_meal_frame.pack()
+        self.create_menu()
+
+    def create_info_frame(self):
+        self.hide_all_frames()
+        info_frame = InfoFrame(self)
+        info_frame.pack()
+        self.create_menu()
+
     def create_welcome_frame(self):
         self.hide_all_frames()
         # label on main Frame
-        self.welcome_label = Label(self, text="Bienvenue sur kotcount")
-        self.welcome_label.place(y=125, x=125)
-        self.welcome_button = ttk.Button(self, text="Créer un kot", command=self.create_kot_frame)
-        self.welcome_button.place(y=150, x=150)
-        self.create_menu()
+        with open("koters.json", "r") as json_f:
+            data = json.load(json_f)
+            if data["kot_name"] == "":
+                welcome_label = Label(self, text="Bienvenue sur kotcount", font="Calibri 15")
+                welcome_label.pack()
+                welcome_button = ttk.Button(self, text="Créer un kot", command=self.create_kot_frame)
+                welcome_button.pack(expand=True)
+                self.create_menu()
+            else:
+                welcome_label = Label(self, text=f"Bienvenue dans le kot {data['kot_name']}", font="Calibri 15")
+                welcome_label.pack(expand=True)
+                self.create_menu()
 
 
 class KotFrame(ttk.Frame):
@@ -97,6 +128,7 @@ class KotFrame(ttk.Frame):
             for v in data["koters_list"]:
                 self.koters_list.append(v)
 
+    @staticmethod
     def add_koter_to_json(self, koter):
         with open("koters.json", "r") as r_json:
             data = json.load(r_json)
@@ -116,16 +148,17 @@ class KotFrame(ttk.Frame):
         meal_frame.pack()
 
     def add_koter(self):
-        self.add_koter_to_json(self.koters_name.get())
+        self.add_koter_to_json(self, self.koters_name.get())
         show_koter = Label(app, text=self.koters_name.get())
         show_koter.pack()
         self.name_entry.delete(0, END)
 
     def delete_koter(self):
-        self.delete_koter_from_json(self.koter_to_delete.get())
+        self.delete_koter_from_json(self, self.koter_to_delete.get())
         self.koter_to_delete_entry.delete(0, END)
         app.create_kot_frame()
 
+    @staticmethod
     def delete_koter_from_json(self, koter):
         with open("koters.json", "r") as r_json:
             data = json.load(r_json)
@@ -143,6 +176,8 @@ class MealFrame(ttk.Frame):
         self.cook_label.pack()
         self.selected_cook = StringVar()
         self.selected_meal = StringVar()
+        self.selected_price = IntVar(value=3)
+        self.groceries_cost = IntVar()
         self.dict_inscription = {}
         self.create_widget()
 
@@ -153,16 +188,60 @@ class MealFrame(ttk.Frame):
             Label(self, text="Repas").pack()
             ttk.Combobox(self, textvariable=self.selected_meal, values=("Matin", "Midi", "Soir"),
                          state="readonly").pack()
+            Label(self, text="Prix").pack()
+            ttk.Entry(self, textvariable=self.selected_price).pack()
             Label(self, text="S'inscrire : ", font='Calibri 14 underline').pack()
             for v in data["koters_list"]:
-                self.dict_inscription[v] = StringVar()
-                ttk.Checkbutton(self, text=v, variable=self.dict_inscription[v]).pack()
-            ttk.Button(self, text="Finaliser le repas", command=self.get_inscription).pack()
+                self.dict_inscription[v] = IntVar()
+                ttk.Checkbutton(self, text=v, variable=self.dict_inscription[v], offvalue=0, onvalue=1).pack()
+            ttk.Button(self, text="Finaliser le repas", command=self.get_inscription).pack(pady=5)
 
     def get_inscription(self):
-        for v, name in zip(self.dict_inscription.values(), self.dict_inscription.keys()):
-            self.dict_inscription[name] = v.get()
-        print(self.dict_inscription)
+        if self.selected_price.get() <= 0:
+            showinfo(title="Attention", message="le prix du repas doivent être des entiers positifs")
+        else:
+            self.dict_inscription[self.selected_cook.get()] = 0
+            for v, name in zip(self.dict_inscription.values(), self.dict_inscription.keys()):
+                if self.selected_cook.get() == name:
+                    continue
+                elif v.get() == 1:
+                    self.dict_inscription[name] = v.get() * self.selected_price.get()
+                    self.dict_inscription[self.selected_cook.get()] -= self.selected_price.get()
+                else:
+                    self.dict_inscription[name] = v.get()
+            app.hide_all_frames()
+            app.create_menu()
+            list_meal_frame = ListMealFrame(app)
+            list_meal_frame.pack()
+
+
+class ListMealFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        self.create_widgets()
+
+    def create_widgets(self):
+        # headers
+        Label(self, text='Inscrits').grid(row=0, column=1, padx=10)
+        Label(self, text='Cuisinier').grid(row=0, column=2, padx=10)
+        Label(self, text='Prix total').grid(row=0, column=3, padx=10)
+        Label(self, text='Prix Course').grid(row=0, column=4, padx=10)
+        Label(self, text='Calcule').grid(row=0, column=5)
+
+        # body
+        Label(self, text='0').grid(row=1, column=0)
+        Label(self, text='1').grid(row=2, column=0)
+        Label(self, text='2').grid(row=3, column=0)
+        Label(self, text='3').grid(row=4, column=0)
+
+
+class InfoFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        self.create_widgets()
+
+    def create_widgets(self):
+        Label(self, text="Koteurs : ").grid(row=0, column=0)
 
 
 if __name__ == "__main__":
